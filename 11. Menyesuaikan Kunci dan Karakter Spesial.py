4e1f3c929b9ccfe5f14a1a4e5ce345511bcecd04
rendhive <rendhivecode@gmail.com>
2026-06-25 18:46:21 +0700

    [DOCT] Sample Library Python: 11. Menyesuaikan Kunci dan Karakter Spesial.py

---
+++ 11. Menyesuaikan Kunci dan Karakter Spesial.py
@@ -0,0 +1,5 @@
+import readline
+
+readline.parse_and_bind("set editing-mode vi")  # Menyetel mode editing ke Vi
+# Fungsi: Mengatur mode pengeditan untuk pengalaman pengguna yang berbeda.
+# Kondisi: Ketika pengguna lebih nyaman dengan pengeditan menggunakan Vi.
