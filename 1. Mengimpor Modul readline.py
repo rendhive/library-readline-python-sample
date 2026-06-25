c4e399fd77e6ce132ebd4b1543140a7e60339b61
rendhive <rendhivecode@gmail.com>
2026-06-25 18:43:09 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul readline.py

---
+++ 1. Mengimpor Modul readline.py
@@ -0,0 +1,5 @@
+import readline
+
+print("Modul readline berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menangani input baris perintah dengan dukungan sejarah.
+# Kondisi: Digunakan sebelum memanfaatkan fitur dari modul readline.
