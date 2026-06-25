21d3cbc1a081993b27b553880c518cf4fd457252
rendhive <rendhivecode@gmail.com>
2026-06-25 18:47:13 +0700

    [DOCT] Sample Library Python: 14. Mengatur Kunci Pintas untuk Perintah.py

---
+++ 14. Mengatur Kunci Pintas untuk Perintah.py
@@ -0,0 +1,6 @@
+import readline
+
+readline.parse_and_bind('tab: complete')
+# Gunakan tombol "tab" untuk menyelesaikan input.
+# Fungsi: Mengatur tombol pintas untuk meningkatkan efisiensi pengguna saat memasukkan perintah.
+# Kondisi: Ketika Anda ingin memudahkan pengguna untuk menyelesaikan perintah saat mengetik.
