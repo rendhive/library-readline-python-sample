7c96cbd57cd556de1a0fc236801182e2ddbd87a5
rendhive <rendhivecode@gmail.com>
2026-06-25 18:43:38 +0700

    [DOCT] Sample Library Python: 2. Menyimpan Input ke Sejarah.py

---
+++ 2. Menyimpan Input ke Sejarah.py
@@ -0,0 +1,7 @@
+import readline
+
+input("Input sesuatu: ")  # Menunggu masukan pengguna
+readline.add_history("Masukan 1")  # Menambahkan input ke sejarah
+print("Input telah disimpan dalam sejarah.")
+# Fungsi: Menyimpan input pengguna ke dalam sejarah untuk digunakan di kemudian hari.
+# Kondisi: Ketika Anda ingin pengguna dapat mengakses kembali masukan sebelumnya.
